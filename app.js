8fe0124c525839acfed5c24ad5cdf835949b1b02
alvinlo92 <alvinlo92@yahoo.com>
2019-07-29 13:05:32 -0700

    Added props

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 // TODO
+var GroceryList = (groceries) => (
+    <ul>
+        <li>{groceries.items[0]}</li>
+        <li>{groceries.items[1]}</li>
+        <li>{groceries.items[2]}</li>
+    </ul>
+);
+
+var GroceryListItem = () => (
+    <div>
+        <h2>Grocery List</h2>
+        <GroceryList items={['Coffee', 'Tea', 'Milk']}/>
+    </div>
+);
+
+// var Coffee = () => (
+//     <li>Coffee</li>
+// );
+
+// var Tea = () => (
+//     <li>Tea</li>
+// );
+
+// var Milk = () => (
+//     <li>Milk</li>
+// );
+
+ReactDOM.render(<GroceryListItem />, document.getElementById("app"));
